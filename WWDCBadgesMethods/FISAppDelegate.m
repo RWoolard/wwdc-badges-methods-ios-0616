078750d61e3634811f5294b54a0b37e4f0d9e631
RWoolard <r.woolard456@gmail.com>
2016-05-13 15:06:50 -0400

    Badges And Rooms

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -21,4 +21,50 @@
  
  */
 
+- (NSString *) badgeForSpeaker:(NSString *)speaker {
+    
+    NSString *welcome = [@"Hello, my name is " stringByAppendingFormat:@"%@.", speaker];
+    
+    return welcome;
+}
+
+- (NSArray *) badgesForSpeakers:(NSArray *)speakers {
+    
+    NSMutableArray *sayHelloSpeakers = [[NSMutableArray alloc] init];
+   
+    for (NSUInteger allSpeakers = 0; allSpeakers < [speakers count] ; allSpeakers++){
+        
+        NSString *sayHello = @"Hello, my name is ";
+        NSString *sayName = [NSString stringWithFormat:@"%@.", speakers[allSpeakers]];
+        NSString *completeBadge = [sayHello stringByAppendingString:sayName];
+        NSLog(@"%@", completeBadge);
+        [sayHelloSpeakers addObject:completeBadge];
+      
+    }
+    
+    return sayHelloSpeakers;
+    
+}
+
+- (NSArray *) greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    
+    NSMutableArray *rooms = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        
+        NSString *welcome = @"Welcome, ";
+        NSString *name = [NSString stringWithFormat:@"%@! ", speakers[i]];
+        NSString *room = @"You'll be in dressing room ";
+        NSString *roomNumber = [NSString stringWithFormat:@"%li.", i + 1];
+        NSString *welcomeSpeaker = [welcome stringByAppendingString:name];
+        NSString *speakersRoom = [room stringByAppendingString:roomNumber];
+        NSString *badgesAndRooms = [welcomeSpeaker stringByAppendingString:speakersRoom];
+        NSLog(@"%@", badgesAndRooms);
+        [rooms addObject:badgesAndRooms];
+        }
+    
+    
+    
+    return rooms;
+}
 @end
